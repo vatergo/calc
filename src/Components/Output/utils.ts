2c5d8b242cd23525e19ab461bdfcce36ab51cc27
vatergo <vatergo@mail.ru>
2024-06-08 22:46:07 +0300

    feat: add c sharp

--- src/Components/Output/utils.ts
+++ src/Components/Output/utils.ts
@@ -1,60 +1,31 @@
-//Вязкость
-export const getViscosity1 = (x: number, y: number) => {
-  return 29.294 + 819.8383 * x - 32.8641 * y;
-};
+import { useEffect, useRef, useState } from "react";
 
-export const getViscosity2 = (x: number, y: number) => {
-  return (
-    -43.621 +
-    2811.9621 * y +
-    365.5409 * x -
-    8351.5551 * Math.pow(y, 2) -
-    2275.8001 * x * y +
-    2564.79 * Math.pow(x, 2)
-  );
-};
+export const useDotNet = (url: string) => {
+  const dotnetUrl = useRef("");
+  const [dotnet, setDotNet] = useState<unknown>(null);
+  const [loading, setLoading] = useState(true);
 
-//Фильтрация
-export const getFiltering1 = (x: number, y: number) => {
-  return (
-    388 -
-    1479.9 * x -
-    920.1 * y +
-    1600.1 * x * y +
-    1599.9 * Math.pow(x, 2) +
-    1000.1 * Math.pow(y, 2)
-  );
-};
+  const load = async (currentUrl: string): Promise<unknown> => {
+    const module = await import(/* @vite-ignore */ currentUrl);
 
-export const getFiltering2 = (x: number, y: number) => {
-  return 440.81 - 1022.388 * x - 757.82 * y;
-};
+    const { getAssemblyExports, getConfig } = await module.dotnet
+      .withDiagnosticTracing(false)
+      .create();
 
-export const getFiltering3 = (x: number, y: number) => {
-  return 1221.67 - 5309.76 * x - 5149.04 * y;
-};
+    const config = getConfig();
+    const exports = await getAssemblyExports(config.mainAssemblyName);
+    return exports;
+  };
 
-//Растекаемость
-export const getSpreadability1 = (x: number, y: number) => {
-  return (
-    220.079 +
-    594.23 * y -
-    54.7803 * x -
-    661.3888 * Math.pow(y, 2) -
-    644.0422 * x * y -
-    81.8865 * Math.pow(x, 2)
-  );
-};
-export const getSpreadability2 = (x: number, y: number) => {
-  return (
-    271.124 +
-    836.3447 * y -
-    464.8676 * x -
-    1326.5561 * Math.pow(y, 2) -
-    905.5659 * x * y +
-    803.4287 * Math.pow(x, 2)
-  );
-};
-export const getSpreadability3 = (x: number, y: number) => {
-  return 348.333 - 916.667 * x + 175.0 * y;
+  useEffect(() => {
+    if (dotnetUrl.current !== url) {
+      // safeguard to prevent double-loading
+      setLoading(true);
+      dotnetUrl.current = url;
+      load(url)
+        .then((exports) => setDotNet(exports))
+        .finally(() => setLoading(false));
+    }
+  }, [url]);
+  return { dotnet, loading };
 };
